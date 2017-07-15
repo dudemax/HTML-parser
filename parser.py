2a5788d1d2a624282ed3cdbd851fe9e40af33f91
root <root@localhost.localdomain>
2017-07-16 00:08:16 +0300

    class support added

--- parser.py
+++ parser.py
@@ -1,17 +1,21 @@
 import urllib2
 from collections import defaultdict
+import checker
 
-def GetTagsData(html, tag):
+def GetTagsData(html, tag, cl):
 	begS=0; endS=0; StagD=''; opent={'holder':0}
-	begE=0; endE=0; EtagD=''; Data=''
+	EtagD=''; Data=''; clM=False;
 	canParse=False
 	myiter = iter(range(1, len(html)))
 	for x in myiter:
-		if html[x]=='<':
+		if html[x]=='<' and html[x+1]!='/' and html[x+1]!='!':
 			compare=''
 			for i in range(1, len(tag)+1):
 				compare +=html[x+i]
+			print(compare)
+			#print(tag)
 			if tag == compare:
+				print('here')
 				if (tag in opent)==False:
 					opent[tag]=0
 				opent[tag]+=1
@@ -20,9 +24,30 @@ def GetTagsData(html, tag):
 					x+=1
 					next(myiter,None)
 					canParse=False
-				canParse=True
+				print(StagD)
+				if cl!=None:
+					if StagD.find('class=')!=-1:
+						print('ble')
+						if StagD[StagD.find('class=')+6]!="\"" and StagD[StagD.find('class=')+6]!='\'':
+							clCompare=''
+							for ci in range(0,len(cl)):
+								clCompare+=StagD[StagD.find('class=') + 6 + ci]
+							print(StagD[StagD.find('class=')+6])
+							print(clCompare)
+							if clCompare==cl:
+								clM=True
+						else:
+							clCompare=''
+							for ci in range(0,len(cl)):
+								clCompare+=StagD[StagD.find('class=') + 7 + ci]
+							if clCompare==cl:
+								clM=True
+				else:
+					clM=True
+				if clM:
+					canParse=True
 				continue
-		if html[x]=='<' and html[x+1]=='/':
+		if html[x]=='<' and html[x+1]=='/' and canParse:
 			compare=''
 			for i in range(2, len(tag)+2):
 				compare +=html[x+i]
@@ -35,6 +60,7 @@ def GetTagsData(html, tag):
 		if canParse:
 			Data+=html[x];
 	return Data
+	
 	
 def GetTableRows(html,rowsC):
 	begS=0; endS=0; StagD=''; opent={'holder':0}
@@ -79,6 +105,10 @@ def GetTableRows(html,rowsC):
 		if canParse:
 			Data+=html[x];
 	return d
+
+def Decode(text):
+	return urllib.unquote(text).decode('utf8')
+	
 	
 
 hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
@@ -87,7 +117,8 @@ hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML,
        'Accept-Encoding': 'none',
        'Accept-Language': 'en-US,en;q=0.8',
        'Connection': 'keep-alive'}
-req = urllib2.Request('https://www.socks-proxy.net/', headers=hdr)
+#req = urllib2.Request('https://www.socks-proxy.net/', headers=hdr)
+req = urllib2.Request('http://freeproxylists.net/', headers=hdr)
 response = urllib2.urlopen(req)
 html = response.read()
 decision='y'
@@ -97,12 +128,32 @@ f.close()
 f = open('out.txt', 'a')
 while decision=='y':
 	tag=raw_input("Enter tag: ")
-	#tagD=GetTagsData(html,tag)
-	tagD=GetTableRows(html,8)
-	for i in range(0, len(tagD[0])):
-		f.write(tagD[4][i].lower()+' '+tagD[0][i]+' '+tagD[1][i]+'\n')
-		print(tagD[4][i].lower()+' '+tagD[0][i]+' '+tagD[1][i])
+	if tag=='td':
+		tagD=GetTableRows(html,10)
+	else:
+		if tag.find('-c=')!=-1:
+			ti=tag.find('-c=')+3
+			cl=''
+			print(tag+' '+str(len(tag)))
+			while ti<len(tag):
+				print(tag[ti])
+				#print(ti)
+				cl+=tag[ti]
+				print(ti)
+				ti+=1
+		tagi=0; tagS=''
+		while tag[tagi]!=' ':
+			tagS+=tag[tagi]
+			tagi+=1
+		print(tagS+' '+cl)
+		tagD=GetTagsData(html,tagS,cl)
+	#for i in range(0, len(tagD[1])):
+		#f.write(tagD[2][i].lower()+' '+tagD[0][i]+' '+tagD[1][i]+'\n')
+		#print(tagD[2][i].lower()+' '+tagD[0][i]+' '+tagD[1][i])
+		#checker.proxyList.append(tagD[0][i]+':'+tagD[1][i])
 		#print(tagD[0][i].lower());
+	#checker.LoopCheck()
+	print(tagD)
 	decision=raw_input("Parse more? (y/n) ")
 	if decision!='y' and decision!='n':
 		print ("Are U kidding at me?!")
